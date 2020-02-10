14a47af0c6080ffcf476dab9039380de560d747e
Cristhian Bolaños <crissebasbol@gmail.co>
2020-02-09 17:43:51 -0800

    Initial Commit

---
+++ Mixit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mixit
+//
+//  Created by Cristhian Bolaños on 2/9/20.
+//  Copyright © 2020 Cristhian Bolaños. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
