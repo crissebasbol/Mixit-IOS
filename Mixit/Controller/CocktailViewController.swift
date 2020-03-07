68f0e583e34d40ebe88e5e58e0c5e1e419da9311
Cristhian Sebastian Bolaños <crissebasbol@gmail.com>
2020-03-06 22:43:39 -0500

    [FEATURE] create UI show cocktails

---
+++ Mixit/Controller/CocktailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CocktailViewController.swift
+//  Mixit
+//
+//  Created by Telematica on 3/6/20.
+//  Copyright © 2020 Cristhian Bolaños. All rights reserved.
+//
+
+import UIKit
+
+class CocktailViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //Method to dismiss the current scene
+    func dismissMe(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
