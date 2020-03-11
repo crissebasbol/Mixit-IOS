f8110cd5b4e6e83c09befa73fa24d7b18ebbca52
Santiago Vasquez <santiago6697@gmail.com>
2020-03-11 01:29:02 -0500

    [FEATURE] Add random cocktail button to CocktailViewController.

--- Mixit/Controller/CocktailViewControllerHome.swift
+++ Mixit/Controller/CocktailViewControllerHome.swift
@@ -13,6 +13,10 @@ class CocktailViewController: UIViewController{
     var delegate: CocktailViewControllerDelegate?
     var cocktail: Cocktail?
     
+    var cocktailsAPIService: CocktailsAPIService = CocktailsAPIService()
+    
+    @IBOutlet weak var image: UIView!
+    
     @IBOutlet weak var descriptionLabel: UILabel!
     
     @IBOutlet weak var tutorialLabel: UILabel!
@@ -21,10 +25,21 @@ class CocktailViewController: UIViewController{
     
     @IBOutlet weak var randomButton: UIView!
     
+    @IBAction func random(_ sender: Any) {
+        cocktailsAPIService.random() {
+            (cocktails, error) in
+            
+            if error != nil {
+                
+            } else if let cocktails = cocktails {
+                self.completeView(cocktail: cocktails[0])
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         if let cocktail = cocktail {
             descriptionLabel.text = cocktail.description
             tutorialLabel.text = cocktail.description
@@ -37,6 +52,7 @@ class CocktailViewController: UIViewController{
                         ingredients = ingredients + ingredient + ", "
                     } else{
                         ingredients = ingredients + ingredient + "."
+
                     }
                 }
                 ingredientsLabel.text = ingredients
@@ -44,7 +60,31 @@ class CocktailViewController: UIViewController{
                 ingredientsLabel.text = ""
             }
             
+        } else {
+            self.random(self)
         }
+    }
+    
+    func completeView (cocktail: Cocktail) -> Void {
+        self.descriptionLabel.text = cocktail.description
+        self.tutorialLabel.text = cocktail.tutorial
+        self.navigationItem.title = cocktail.title
+        self.randomButton.isHidden = false
+        // self.image.addSubview( UIImageView(image: cocktails[0].image))
+        var ingredients = ""
+        if cocktail.ingredients.count > 0 {
+            for (index, ingredient) in cocktail.ingredients.enumerated(){
+                if index+1 != cocktail.ingredients.count{
+                    ingredients = ingredients + ingredient + ", "
+                } else{
+                    ingredients = ingredients + ingredient + "."
+                }
+            }
+            self.ingredientsLabel.text = ingredients
+        } else{
+            self.ingredientsLabel.text = ""
+        }
+    
     }
     
     override func didReceiveMemoryWarning() {
